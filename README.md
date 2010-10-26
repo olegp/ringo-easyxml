0a655d2e8028bf4af2a337070f8033f59e396cc3
Oleg Podsechin <oleg.podsechin@ionsquare.com>
2010-10-26 23:04:43 +0200

    changed e4x to xml throughout

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
 # ringo-easyxml
 
-One way converter from E4X XML to JSON. Usage:
+One way converter from XML to JSON. Usage:
 
-    var {E4XtoJSON} = require("easyxml");
+    var {XMLtoJSON} = require("easyxml");
     var xml = '<a><b>c</b></a>';
-    var json = E4XtoJSON(xml);
+    var json = XMLtoJSON(xml);
 
 Turns child nodes with the same name into arrays so that lists are easier
 to work with.
 
-    <body><item>1</item><item>2</item></body>
-
-    body: {item: ["1", "2"]}
+`<body><item>1</item><item>2</item></body>` becomes `body: {item: ["1", "2"]}`
      
 Turns XML node attributes into object attributes,
 the children are placed inside a special attribute named "_":
 
-    <body a="a">whatever</body>
+`<body a="a">whatever</body>` becomes `body: {_a: "a", _: "whatever"}`
 
-    body: {_a: "a", _: "whatever"}
-
-For other options, take a look at test/all.js.
+For options, take a look at testOptions in `test/all.js.`
 
--- test/all.js
+++ test/all.js
@@ -1,6 +1,6 @@
 include('assert');
 
-var {E4XtoJSON} = require("easyxml");
+var {XMLtoJSON} = require("easyxml");
 var {get} = require("ringo/httpclient");
 
 exports.setUp = function () {
@@ -8,36 +8,36 @@ exports.setUp = function () {
 
 exports.testBasic = function() {
   var xml = '<a><b>c</b></a>';
-  var json = E4XtoJSON(xml);
+  var json = XMLtoJSON(xml);
   equal(json.b, "c");
 }
 
 exports.testAttribute = function() {
   var xml = '<a><b c="d"/></a>';
-  var json = E4XtoJSON(xml);
+  var json = XMLtoJSON(xml);
   equal(json.b._c, "d");
 }
 
 exports.testOptions = function() {
   var xml = '<a><b c="d">e</b></a>';
   
-  var json1 = E4XtoJSON(xml);
+  var json1 = XMLtoJSON(xml);
   equal(json1.b._c, "d");
   equal(json1.b._, "e");
   
-  var json2 = E4XtoJSON(xml, {simple: ["b"]});
+  var json2 = XMLtoJSON(xml, {simple: ["b"]});
   equal(json2.b, "e");
   
-  var json3 = E4XtoJSON(xml, {ignored: ["c"]});
+  var json3 = XMLtoJSON(xml, {ignored: ["c"]});
   equal(json3.b, "e");
   
-  var json4 = E4XtoJSON(xml, {prefix: ""});
+  var json4 = XMLtoJSON(xml, {prefix: ""});
   equal(json4.b.c, "d");
 }
 
 exports.testCDATA = function() {
   var xml = '<a><b><![CDATA[<c>d</c>]]></b></a>';
-  var json = E4XtoJSON(xml);
+  var json = XMLtoJSON(xml);
   equal(json.b, "<c>d</c>");
 }
 
